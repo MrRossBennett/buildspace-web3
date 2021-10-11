d6fb37f437ad1b1bcb7ba251a910d897cadce077
Ross Bennett <rossdis@gmail.com>
2021-10-11 16:55:03 +0100

    Add SVG

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div className="w-full bg-gradient-to-t from-gray-800 to-indigo-900 min-h-screen antialiased">
+      <svg class="absolute w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1685.86 387.16"><polygon points="1683.97 0.5 1005.34 386.66 645.6 386.66 1.81 0.5 1683.97 0.5" fill="none" stroke="#1E3A8A"></polygon><path d="M282.17.5,705.55,386.66" fill="none" stroke="#1E3A8A"></path><path d="M562.53.5l203,386.16" fill="none" stroke="#1E3A8A"></path><path d="M842.89.5,825.47,386.66" fill="none" stroke="#1E3A8A"></path><path d="M1123.25.5,885.43,386.66" fill="none" stroke="#1E3A8A"></path><path d="M1403.61.5,945.39,386.66" fill="none" stroke="#1E3A8A"></path><path d="M1033.64,370.69h-414" fill="none" stroke="#1E3A8A"></path><path d="M1071.64,349.69h-488" fill="none" stroke="#1E3A8A"></path><path d="M1125.64,318.69h-593" fill="none" stroke="#1E3A8A"></path><path d="M1208.64,270.69h-756" fill="none" stroke="#1E3A8A"></path></svg>
       <Head>
         <title>Adventures in Ethereum</title>
 
@@ -13,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="w-full max-w-screen-xl mx-auto text-white flex flex-col justify-center py-24 px-6 font-body">
+      <main className="w-full max-w-screen-xl mx-auto text-white flex flex-col justify-center py-24 px-6 font-body relative">
         <header class="text-center mb-20">
           <h1 class="text-4xl font-display mb-6">Adventures in Ethereum</h1>
           <p class="text-xl tracking-wide">What happens next?</p>
@@ -24,7 +25,7 @@ export default function Home() {
             <h2 class="font-display">Create your snippet</h2>
             <label class="block text-gray-700 my-8">
               <span class="sr-only">Snippet</span>
-              <textarea class="mt-1 block w-full p-3 rounded-lg bg-indigo-900 resize-none text-white placeholder-indigo-200" rows="6" placeholder="Add snippet..."></textarea>
+              <textarea class="mt-1 block w-full p-3 rounded-lg bg-indigo-900 resize-none text-white placeholder-indigo-200 border" rows="6" placeholder="Add snippet..."></textarea>
             </label>
             <button class="w-full sm:w-auto flex-none bg-pink-500 hover:bg-pink-600 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-pink-600 focus:outline-none transition-colors duration-200 inline-flex items-center justify-center">
               <span class="mr-3">Let's go!</span>
